6e1209afcdd286ff1f298f2fe9eaaa9b3a0716ba
vasilis gavrielatos <vasigavr1@gmail.com>
2021-09-15 16:18:18 +0100

    updates after semedi

--- bin/create_houston_sshfs.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-cd ~/Documents
-rm -rf houston
-mkdir houston
-sshfs houston-tun:/home/s1687259 ./houston/
---
+++ bin/create_semedi_sshfs.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+cd ~/Documents
+rm -rf semedi
+mkdir semedi
+sshfs semedi:/home/vasilis ./semedi/
